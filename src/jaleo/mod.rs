b587f58fe43fd8e9901bd867edb481d734badb58
Ivan Litteri <67517699+ilitteri@users.noreply.github.com>
2022-12-14 12:40:39 -0300

    Move Transition logic to a module (#61)
    * Move transition logic to a module
    
    * Update simpleworks package
    
    * cargo fmt
    
    * Add ssl to the coverage job
    
    * Fix coverage job runs
    
    * Fix CI

--- src/jaleo/mod.rs
+++ src/jaleo/mod.rs
@@ -1,8 +1,6 @@
 use std::str::FromStr;
 
-use crate::variable_type::VariableType;
 use anyhow::{anyhow, Result};
-use serde::{Deserialize, Serialize};
 pub use snarkvm::prelude::Itertools;
 use snarkvm::prelude::Testnet3;
 
@@ -10,11 +8,17 @@ mod execute;
 pub use execute::{credits_execution, generate_execution, verify_execution};
 
 mod deploy;
-pub use deploy::{deserialize_verifying_key_map, generate_deployment, serialize_verifying_key_map, verify_deployment, Deployment, VerifyingKeyMap};
+pub use deploy::{
+    deserialize_verifying_key_map, generate_deployment, serialize_verifying_key_map,
+    verify_deployment, Deployment, VerifyingKeyMap,
+};
 
 mod record;
 // Rename to Record when we get rid of snarkVM's.
 pub use record::Record as JAleoRecord;
+
+mod transition;
+pub use transition::Transition;
 
 pub type Address = snarkvm::prelude::Address<Testnet3>;
 pub type Identifier = snarkvm::prelude::Identifier<Testnet3>;
@@ -30,61 +34,6 @@ pub type Origin = snarkvm::prelude::Origin<Testnet3>;
 pub type Output = snarkvm::prelude::Output<Testnet3>;
 pub type ProgramID = snarkvm::prelude::ProgramID<Testnet3>;
 pub type VerifyingKey = snarkvm::prelude::VerifyingKey<Testnet3>;
-
-#[derive(Clone, Serialize, Deserialize, Debug)]
-pub struct Transition {
-    // /// The transition ID.
-    // id: String,
-    /// The program ID.
-    pub program_id: String,
-    /// The function name.
-    function_name: String,
-    /// The transition inputs.
-    inputs: Vec<VariableType>,
-    /// The transition outputs.
-    outputs: Vec<VariableType>,
-    // /// The inputs for finalize.
-    // finalize: Option<Vec<Value>>,
-    /// The transition proof.
-    proof: String,
-    // /// The transition public key.
-    // tpk: Group,
-    // /// The transition commitment.
-    // tcm: Field,
-    /// The network fee.
-    pub fee: i64,
-}
-
-impl Transition {
-    pub fn output_records(&self) -> Vec<VariableType> {
-        self.outputs
-            .clone()
-            .into_iter()
-            .filter(|o| matches!(o, VariableType::Record(..)))
-            .collect()
-    }
-
-    pub fn origins(&self) -> Vec<String> {
-        self.input_records()
-            .iter()
-            .filter_map(|r| {
-                if let VariableType::Record(_serial_number, origin, _record) = r {
-                    Some(origin.clone())
-                } else {
-                    None
-                }
-            })
-            .collect()
-    }
-
-    fn input_records(&self) -> Vec<VariableType> {
-        self.inputs
-            .clone()
-            .into_iter()
-            .filter(|o| matches!(o, VariableType::Record(..)))
-            .collect()
-    }
-}
 
 pub fn program_is_coinbase(program_id: &str, function_name: &str) -> bool {
     (function_name == "mint" || function_name == "genesis") && program_id == "credits.aleo"
---
+++ src/jaleo/transition.rs
@@ -0,0 +1,57 @@
+use crate::variable_type::VariableType;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct Transition {
+    // /// The transition ID.
+    // id: String,
+    /// The program ID.
+    pub program_id: String,
+    /// The function name.
+    pub function_name: String,
+    /// The transition inputs.
+    pub inputs: Vec<VariableType>,
+    /// The transition outputs.
+    pub outputs: Vec<VariableType>,
+    // /// The inputs for finalize.
+    // finalize: Option<Vec<Value>>,
+    /// The transition proof.
+    pub proof: String,
+    // /// The transition public key.
+    // tpk: Group,
+    // /// The transition commitment.
+    // tcm: Field,
+    /// The network fee.
+    pub fee: i64,
+}
+
+impl Transition {
+    pub fn output_records(&self) -> Vec<VariableType> {
+        self.outputs
+            .clone()
+            .into_iter()
+            .filter(|o| matches!(o, VariableType::Record(..)))
+            .collect()
+    }
+
+    pub fn origins(&self) -> Vec<String> {
+        self.input_records()
+            .iter()
+            .filter_map(|r| {
+                if let VariableType::Record(_serial_number, origin, _record) = r {
+                    Some(origin.clone())
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    fn input_records(&self) -> Vec<VariableType> {
+        self.inputs
+            .clone()
+            .into_iter()
+            .filter(|o| matches!(o, VariableType::Record(..)))
+            .collect()
+    }
+}
