269cc96260740ab63d5cfcd9a166c70d69f9974a
Pablo Deymonnaz <deymonnaz@gmail.com>
2023-01-16 19:11:16 -0300

    firsts steps

--- src/jaleo/record.rs
+++ src/jaleo/record.rs
@@ -242,6 +242,47 @@ impl Record {
         self.owner == *address
     }
 
+    // #############################################################################
+    /// Generate a `(SecretKey, PublicKey)` pair
+    pub fn generate_keypair() -> (SecretKey, PublicKey) {
+        let sk = SecretKey::random(&mut thread_rng());
+        (sk, PublicKey::from_secret_key(&sk))
+    }
+
+    /// Encrypt a message by a public key
+    ///
+    /// # Arguments
+    ///
+    /// * `receiver_pub` - The u8 array reference of a receiver's public key
+    /// * `msg` - The u8 array reference of the message to encrypt
+    pub fn encrypt(&self, view_key: &Address) -> Result<EncryptedRecord> {
+        let receiver_pk = PublicKey::parse_slice(receiver_pub, None)?;
+        let (ephemeral_sk, ephemeral_pk) = generate_keypair();
+
+        let aes_key = encapsulate(&ephemeral_sk, &receiver_pk)?;
+        let encrypted = aes_encrypt(&aes_key, msg).ok_or(SecpError::InvalidMessage)?;
+
+        let mut cipher_text = Vec::with_capacity(FULL_PUBLIC_KEY_SIZE + encrypted.len());
+        cipher_text.extend(ephemeral_pk.serialize().iter());
+        cipher_text.extend(encrypted);
+
+        Ok(cipher_text)
+    }
+
+    /// Calculate a shared AES key of our secret key and peer's public key by hkdf
+    pub fn encapsulate(sk: &SecretKey, peer_pk: &PublicKey) -> Result<AesKey, SecpError> {
+        let mut shared_point = *peer_pk;
+        shared_point.tweak_mul_assign(sk)?;
+
+        let mut master = Vec::with_capacity(FULL_PUBLIC_KEY_SIZE * 2);
+        master.extend(PublicKey::from_secret_key(sk).serialize().iter());
+        master.extend(shared_point.serialize().iter());
+
+        hkdf_sha256(master.as_slice())
+    }
+
+    // #############################################################################
+
     pub fn encrypt(&self, view_key: &ViewKey) -> Result<EncryptedRecord> {
         let aes_key = Aes128::new_from_slice(
             view_key
