821ce54eed2fbb8008516ac918e58959f9521696
Javier Chatruc <jrchatruc@gmail.com>
2023-01-23 13:03:48 -0300

    Use seed when minting record

--- src/jaleo/mod.rs
+++ src/jaleo/mod.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 
 use anyhow::{anyhow, Result};
+use rand::SeedableRng;
 use simpleworks::{
     gadgets::ConstraintF,
     marlin::{generate_rand, MarlinProof},
@@ -66,17 +67,23 @@ pub fn generate_program(program_string: &str) -> Result<Program> {
 /// Generate a credits record of the given amount for the given owner,
 /// by using the given seed to deterministically generate a nonce.
 pub fn mint_credits(
-    owner_view_key: &ViewKey,
+    owner_address: &Address,
     credits: u64,
-    _seed: u64,
+    seed: u64,
 ) -> Result<(Field, EncryptedRecord)> {
     let mut address = [0_u8; 63];
-    let owner_address = Address::try_from(owner_view_key)?.to_string();
-    for (address_byte, owner_address_byte) in address.iter_mut().zip(owner_address.as_bytes()) {
+    for (address_byte, owner_address_byte) in
+        address.iter_mut().zip(owner_address.to_string().as_bytes())
+    {
         *address_byte = *owner_address_byte;
     }
 
-    let rng = &mut rand::thread_rng();
+    let mut seed_bytes: [u8; 32] = [0; 32];
+    for (index, byte) in seed_bytes.iter_mut().enumerate() {
+        *byte = ((seed >> index) & 1).try_into()?;
+    }
+
+    let rng = &mut rand::rngs::StdRng::from_seed(seed_bytes);
     let randomizer = Scalar::rand(rng);
 
     let mut non_encrypted_record = Record::new(address, credits, RecordEntriesMap::default(), None);
